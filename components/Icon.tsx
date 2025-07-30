1f767851ece8770a2b4dac5d036fb4094ff092c5
dcannell5 <136128349+dcannell5@users.noreply.github.com>
2025-07-30 04:51:51 -0600

    Update Icon.tsx

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -368,3 +368,11 @@ export const IconDownload: React.FC<IconProps> = ({ className }) => (
     <line x1="12" y1="15" x2="12" y2="3" />
   </svg>
 );
+
+export const IconPrinter: React.FC<IconProps> = ({ className }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+    <polyline points="6 9 6 2 18 2 18 9" />
+    <path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2" />
+    <rect x="6" y="14" width="12" height="8" />
+  </svg>
+);
