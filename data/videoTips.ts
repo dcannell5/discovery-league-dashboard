4198601469566efc78e681623c3c9269b0ed4fb5
dcannell5 <136128349+dcannell5@users.noreply.github.com>
2025-08-07 02:22:53 -0600

    Update videoTips.ts

--- data/videoTips.ts
+++ data/videoTips.ts
@@ -1,17 +1,1 @@
-// Curated list of YouTube video IDs from the "Youth Volleyball - empower the future" playlist
-// https://www.youtube.com/playlist?list=PLcmET8OvKcJ-MJdEqUe968PBvnFSZecll
-// Note: Many videos from the original playlist have been made private or unavailable. This list contains only the currently accessible videos.
-export const videoTips: string[] = [
-  "rL31ivzH22Y", "KzDtjB3pBxk", "sZpQy0-fX98", "yXqg2rKx9AY", "Tj0r1VlJ264",
-  "8L3_Mh0o-4Y", "eHFTVEMxpx8", "Sg5H4GlxI-I", "S3aXyGysIbs", "dYt_XorM-gQ",
-  "U03jA2RkGeM", "7B4gZE8A5o8", "b5a9bT4252w", "f8VzM-J_AE8", "G3MhvlWSAjM",
-  "9YQfTI2Ld30", "dM9cDK0sM-s", "qQfWwX00tIM", "Z2w9lztBD84", "07vNCSf-yzg",
-  "L3l-hQc53zI", "rYQy8oYqgDc", "u5k4h0J-qM4", "S80qfY5Fh-M", "xYn7Ljd2Y7Q",
-  "YjRjK1TNyU8", "wF5f-461d3I", "zI6T6aB-xGQ", "gqXqEA-4p2c", "t8wYdC9u6sc",
-  "oVimO3gIads", "RBLTsqS5TQE", "6OQ2eYp16pM", "Y9tXgs38f-E", "oF-J6iOqSjM",
-  "WcW2f2g5Isw", "QYyV1pY73oM", "y23n-PzPFRk", "wY8h1sE_v9Q", "K0g3bJbH8tE",
-  "Jd5a9oHviVw", "eJd_8z_ZcMQ", "33282-Nf4F4", "3T9-j1iL_yU", "p390QYt05Lw",
-  "W0_a-5wEa-Y", "K9Tf_L4wNsc", "8oZ3LdG6a4E", "uVeyBw5e2oY", "lC_5SIsR8mY",
-  "jO5tqA7Ff4Y", "iBTRxU6DABs", "3_7t8xG1Tq8", "P7J-nZJp_mY", "uF08oFj4O3U",
-  "v6Y2KAS8cvo"
-];
+// This file is unused. Links to video resources are handled within components.
